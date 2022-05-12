09acd27e1c7a6a6b2d6126a0f86ffd33f77e5f44
rvsverchkov <s10z40@yandex.ru>
2022-05-12 09:29:36 +0300

    Инициализация CRA

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <header className="header">
+            <h1 className="header__title">Spot</h1>
+            <button className="header__button">Войти</button>
+        </header>
+    )
+}
+
+export default Header;
